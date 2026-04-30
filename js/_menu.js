77069ceab405ae814bf14d336d5642f2084b0a56
Aftab Azeem <aftab.azeeem@icloud.com>
2026-04-30 10:11:30 +0200

    Simplified menu to single dashboard

--- js/_menu.js
+++ js/_menu.js
@@ -1,6 +1,3 @@
-createMenu('Statistics Template JS', [
-  { name: 'Välkommen', script: 'welcome.js' },
-  { name: 'Visa ett år', script: 'one-year.js' },
-  { name: 'Jämför två år', script: 'compare-two-years.js' },
-  { name: 'Hitta trender', script: 'trends.js' }
+createMenu('Student Depression Analysis', [
+  { name: 'Dashboard', script: 'one-year.js' }
 ]);
